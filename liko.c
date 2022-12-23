e884569d9f23b00a133f5695b082d89115b59771
Ivan Zdane <mail@ivanzdane.com>
2022-12-23 15:54:43 -0800

    Enable/disable tty raw mode

--- liko.c
+++ liko.c
@@ -1,3 +1,80 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void die(const char* s) {
+	perror(s);
+	exit(1);
+}
+
+void disable_raw_mode() {
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+		die("tcsetattr");
+}
+
+void enable_raw_mode() {
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+		die("tcgetattr");
+	atexit(disable_raw_mode);
+
+	struct termios raw = orig_termios;	
+
+	// Input flags:
+	// IXON: Disable XON and XOFF signals (Ctrl-S and Ctrl-Q)
+	// ICRNL: Disable turning carriage returns into new lines
+	// BRKINT: Disable SIGINT on break condititons?
+	// INPCK: Disable parity checking
+	// ISTRIP: Disable bit stripping
+	// IXON: Disable start/stop output control
+	raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP | IXON);
+
+	// Output flags:
+	// OPOST: disable output post processing
+	raw.c_oflag &= ~(OPOST);
+
+	// Control flags:
+	// CS8: Set 8 bit characters
+	raw.c_cflag |= ~(CS8);
+
+	// Local flags:
+	// ECHO: disable text from being printed back to the screen
+	// ICANON: disable cannonical mode (input being read in one line at a time)
+	// ISIG: disable SIGINT, SIGSTP signals
+	// IEXTEN: disable Ctrl-V sending literal characters instead of control characters
+	// 			and disable Ctrl-O from discarding control characters
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+	// Control characters:
+	// VMIN: maximum number of bytes needed before read returns
+	// VTIME: maximum amount of time in 1/10s to wait before read returns
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+		die("tcsetattr");
+}
+
 int main() {
+	enable_raw_mode();
+
+	while (1) {
+		char c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+			die("read");
+
+		if (iscntrl(c)) {
+			printf("%d \r\n", c);
+		} else {
+			printf("%d %c \r\n", c, c);
+		} 
+		if (c == 'q') break;
+	}
+
 	return 0;
 }
